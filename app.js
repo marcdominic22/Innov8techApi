31cf64c1ad668779b840fbef62c719276230b9b3
Marc Terrobias <marci.terrobias@gmail.com>
2024-10-25 04:20:51 +0800

    try 2nd

--- app.js
+++ app.js
@@ -27,47 +27,50 @@ app.use(bodyParser.json());
 // Access environment variables
 const port = config.port;
 
-
-const swaggerOptions = {
-    swaggerDefinition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Innov8Tech API',
-        version: '1.0.0',
-        description: 'Innov8Tech API with Swagger documentation',
-        contact: {
-          name: 'Innov8Tech Dev Team',
-          url: 'https://innov8tech.co', // Optional URL for the contact
-          email: 'dev@innov8tech.com'
-        },
-        version: "v1",
-        servers: [
-            {
-              url: `http://localhost:${port}/v1`, // Dynamic server URL with port
-              description: 'Version 1 of API',
-            },
-          ],
-      },
-    },
-    apis: ['./routes/v1/ghl/*.js'], // Path to the API docs (this file)
-    // apis: ['./routes/**/*.js'], // Path to all the API docs
-  };
+// const swaggerOptions = {
+//     swaggerDefinition: {
+//       openapi: '3.0.0',
+//       info: {
+//         title: 'Innov8Tech API',
+//         version: '1.0.0',
+//         description: 'Innov8Tech API with Swagger documentation',
+//         contact: {
+//           name: 'Innov8Tech Dev Team',
+//           url: 'https://innov8tech.co', // Optional URL for the contact
+//           email: 'dev@innov8tech.com'
+//         },
+//         version: "v1",
+//         servers: [
+//             {
+//               url: `http://localhost:${port}/v1`, // Dynamic server URL with port
+//               description: 'Version 1 of API',
+//             },
+//           ],
+//       },
+//     },
+//     apis: ['./routes/v1/ghl/*.js'], // Path to the API docs (this file)
+//     // apis: ['./routes/**/*.js'], // Path to all the API docs
+//   };
   
-  // Swagger docs setup
-  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+//   // Swagger docs setup
+//   const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-  // Serve swagger.json file
-  app.get('/swagger.json', (req, res) => {
-    res.json(swaggerDocs);
-  });
+//   // Serve swagger.json file
+//   app.get('/swagger.json', (req, res) => {
+//     res.json(swaggerDocs);
+//   });
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
-    explorer: true, // Enable API explorer
-  }));
+//   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
+//     explorer: true, // Enable API explorer
+//   }));
 
-  app.get('/', (req, res) => {
-    res.redirect('/api-docs');
-  });
+//   app.get('/', (req, res) => {
+//     res.redirect('/api-docs');
+//   });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'openapi.html'));
+});
 
   app.use('/v1', v1Routes);  // API v1 routes
 
@@ -83,10 +86,6 @@ const swaggerOptions = {
     console.error(err.stack)
     res.status(500).send('Internal Server Error')
   })
-
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'openapi.html'));
-});
 
   // const options = {
   //   key: fs.readFileSync('key.pem'),
